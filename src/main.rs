8b0794febbbdcc8b22e8e787a404bb61364a3b90
chelsea <chelseawen03@gmail.com>
2024-05-11 21:41:20 -0700

    from PR to TDX Sample Data github, added HashMap to hide auth IDs

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use core::result::Result;
+use reqwest::{header::CONTENT_TYPE, header::AUTHORIZATION, *};
+use serde_json::*;
+use std::error::Error;
+use std::{collections::HashMap, fs::File, path::Path};
+
+static AUTH_URL: &str = "https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token";
+static TEST_URL: &str = "https://tdx.transportdata.tw/api/basic/v2/Rail/TRA/LiveBoard/Station/1000?$filter=Direction eq 1&$format=JSON";
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    let file_path = Path::new("C:\\Users\\bookw\\Downloads\\tdx-secret.json");
+    let file = File::open(file_path).expect("file not found");
+    let secret: HashMap<String, String> =
+        serde_json::from_reader(file).expect("error while reading");
+
+    let auth_header = json!({
+        "grant_type": "client_credentials",
+        "client_id": secret.get("client_id").unwrap(),
+        "client_secret": secret.get("client_secret").unwrap()
+    });
+
+    let client = Client::new();
+    let auth_response = client
+        .post(AUTH_URL)
+        .header(CONTENT_TYPE, "application/x-www-form-urlencoded")
+        .form(&auth_header)
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    let data_header = auth_response.split_once("\":\"").unwrap().1;
+    let access_token = format!("Bearer {}", data_header.split_once("\",").unwrap().0);
+
+    let data_response = client
+        .get(TEST_URL)
+        .header(AUTHORIZATION, access_token)
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    println!("{:?}", data_response);
+
+    Ok(())
+}
